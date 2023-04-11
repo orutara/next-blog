36ef1109d7ed389d6c0ea8a94fd1692b2cb44297
orutara <orutaracom@gmail.com>
2023-04-12 00:53:55 +0900

    🔧 colors追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,11 @@ module.exports = {
     colors: {
       'white': '#ffffff',
       'black': '#333333',
+      'mat': '#7B7B7B',
       'dark': '#475050',
       'primary': '#42B983',
-      'sub': '#EFFFF8'
+      'sub': '#EFFFF8',
+      'link': '#4773BB'
     },
     extend: {
     },
