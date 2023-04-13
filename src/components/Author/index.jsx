59003475ca0c8cecb0183c3831ecb944d3855a6e
orutara <orutaracom@gmail.com>
2023-04-13 22:14:01 +0900

    ♻️ 属性修正

--- src/components/Author/index.jsx
+++ src/components/Author/index.jsx
@@ -1,10 +1,10 @@
 export const Author = () => {
   return (
-    <div class="bg-white py-8 px-5 mb-8">
-      <p class="text-dark text-center mb-4 font-bold text-[20px]">
-        <span itemprop="name">タイラ</span>
+    <div className="bg-white py-8 px-5 mb-8">
+      <p className="text-dark text-center mb-4 font-bold text-[20px]">
+        <span itemProp="name">タイラ</span>
       </p>
-      <p class="text-xs leading-7 text-mat">
+      <p className="text-xs leading-7 text-mat">
         こちらに自己紹介が入ります。こちらに自己紹介が入ります。こちらに自己紹介が入ります。こちらに自己紹介が入ります。
       </p>
     </div>
