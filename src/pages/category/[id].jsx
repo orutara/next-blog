c0cf91e4c7b2fe2e1bb104975902046cd9380fbc
orutara <orutaracom@gmail.com>
2023-04-15 15:08:50 +0900

    ✨ カテゴリーページの実装

--- src/pages/category/[id].jsx
+++ src/pages/category/[id].jsx
@@ -1,17 +1,28 @@
 import Link from "next/link";
+import Image from "next/image";
 import { client } from "@/libs/client";
 
-const CategoryId = ({ blog }) => {
+const CategoryId = ({ blog, category }) => {
   // カテゴリーに紐付いたコンテンツがない場合に表示
   if (blog.length === 0) {
     return <div>ブログコンテンツがありません</div>;
   }
   return (
     <div>
-      <ul>
+      {/* <h2>
+        {category.name}
+      </h2> */}
+      <ul className="mb-16 md:flex md:flex-wrap">
         {blog.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+          <li key={blog.id} className="flex-[0_1_50%] px-4 pb-8">
+            <article className="bg-white shadow-md transition duration-300 hover:shadow hover:translate-y-[1px]">
+              <Link href={`/blog/${blog.id}`}>
+                <Image className="object-cover object-center w-full h-[200px]" src={blog.ogimage.url} width={blog.ogimage.width} height={blog.ogimage.height} alt={blog.title} />
+              </Link>
+              <div className="bg-white p-[16px] min-h-[88px] text-[15px]">
+                <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+              </div>
+            </article>
           </li>
         ))}
       </ul>
@@ -31,10 +42,13 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context) => {
   const id = context.params.id;
   const data = await client.get({ endpoint: "blog", queries: { filters: `category[equals]${id}` } });
-
+  // const categoryData = await client.get({ endpoint: "categories", queries: { filters: `category[equals]${id}` } });
+  // console.log(categoryData);
+  
   return {
     props: {
       blog: data.contents,
+      // category: categoryData
     },
   };
 };
