b610cd6d8b9dafccb8de6e4686f27bb66a85f893
orutara <orutaracom@gmail.com>
2023-04-23 01:41:19 +0900

    ✨ フッターの注釈を追加

--- src/components/organisms/Footer/index.jsx
+++ src/components/organisms/Footer/index.jsx
@@ -1,9 +1,17 @@
 export const Footer = () => {
   return (
-    <footer className="bg-dark py-4">
-      <div className="text-center">
-        <p className="text-white">© 2023 JSgraph</p>
+    <footer>
+      <div className="bg-white">
+        <p className="text-[12px] text-center py-2">
+          LPgraph では Storyset 様の素材を使用させていただいております<br />
+          <a href="https://storyset.com" target="_blank" rel="noreferrer,noopener" className="text-[#1273EB]">illustrations by Storyset</a>
+        </p>
+      </div>
+      <div className="bg-dark py-4">
+        <div className="text-center">
+          <p className="text-white text-[14px]">© 2023 LPgraph</p>
+        </div>
       </div>
     </footer>
-  )
-}
+  );
+};
