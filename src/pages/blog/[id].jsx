2f343159e97ea07d197b0d77a344937b2f070e96
orutara <orutaracom@gmail.com>
2023-04-23 01:46:52 +0900

    ⚡️ 主に見出しのデザインを調整

--- src/pages/blog/[id].jsx
+++ src/pages/blog/[id].jsx
@@ -15,24 +15,22 @@ const BlogId = ({ blog }) => {
   return (
     <>
       <Head>
-        <title>{blog.title} | JSgraph - フロントエンド生存戦略</title>
+        <title>{blog.title} | LPgraph - フロントエンド生存戦略</title>
         <meta name="description" content={blog.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <div className="bg-white py-14 mb-16">
+      <div className="bg-white py-14 mb-16 mx-4 lg:mx-0">
         <p className="text-[13px] text-center text-mat mb-[20px] tracking-wider">
           {day}
         </p>
-        <h1 className="text-dark text-center text-[28px] font-bold mb-[16px]">
+        <h1 className="text-dark text-center text-[18px] sm:text-[24px] font-bold leading-loose tracking-wider antialiased mb-[16px] px-4 lg:px-0 lg:mx-[40px]">
           {blog.title}
         </h1>
         <p className="text-[13px] text-link text-center mb-[32px]">
-          <Link href={`/category/${blog.category && blog.category.name}`}>
-            {blog.category && blog.category.name}
-          </Link>
+          <Link href={`/category/${blog.category.id}`}>{blog.category.id}</Link>
         </p>
         <Image
-          src={blog.ogimage.url}
+          src={blog.ogimage?.url ?? "/img_noimage.png"}
           alt={blog.title}
           className="w-full max-h-[450px] object-cover object-center mb-[40px]"
           width={750}
