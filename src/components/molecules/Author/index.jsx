f75d778476da744643a35e80a95a6a56b17cd91f
orutara <orutaracom@gmail.com>
2023-04-23 14:42:30 +0900

    🍱 管理人情報更新

--- src/components/molecules/Author/index.jsx
+++ src/components/molecules/Author/index.jsx
@@ -1,11 +1,16 @@
+import Image from "next/image";
+
 export const Author = () => {
   return (
-    <div className="bg-white py-8 px-5 mb-8">
+    <div className="bg-white py-10 px-5 mb-8">
+      <div className="mb-8">
+        <Image src="/img_profile.png" alt={'管理人'} width={130} height={130} className="rounded-[50%] mx-auto" />
+      </div>
       <p className="text-dark text-center mb-4 font-bold text-[20px]">
         <span itemProp="name">タイラ</span>
       </p>
-      <p className="text-xs leading-7 text-mat">
-        こちらに自己紹介が入ります。こちらに自己紹介が入ります。こちらに自己紹介が入ります。こちらに自己紹介が入ります。
+      <p className="text-xs leading-7 text-mat tracking-wider">
+        LPgraphの管理人 | 30歳 | 生まれも育ちも北海道。フロントエンドエンジニア兼Webデザイナー兼SEO担当（歴合計3年）。
       </p>
     </div>
   );
