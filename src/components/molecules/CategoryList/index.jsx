792c7f69b58a2c8ead53a65291eed96c96e1d4ff
orutara <orutaracom@gmail.com>
2023-05-06 00:41:16 +0900

    ⚡️ 使用しない箇所をコメントアウト

--- src/components/molecules/CategoryList/index.jsx
+++ src/components/molecules/CategoryList/index.jsx
@@ -1,35 +1,35 @@
-import Link from "next/link"
+// import Link from "next/link"
 
-export const CategoryList = ({ category }) => {
+// export const CategoryList = ({ category }) => {
   
-  return (
-    <div className="bg-white py-8 px-5 mb-8">
-    <p className="text-center mb-4 font-bold">Category</p>
-    <ul className="text-sm leading-5">
-      {
-        !category 
-        ? `Loading...`
-        : category.map((category) => {
-          return ( <li key={category.id}>
-          <Link href={`/category/${category.id}`} legacyBehavior>
-            <a className="block py-2 my-1 pl-2 hover:bg-hover">{category.name}</a>
-          </Link>
-        </li>
-        );
-      })}
-    </ul>
-  </div>
-  );
-};
+//   return (
+//     <div className="bg-white py-8 px-5 mb-8">
+//     <p className="text-center mb-4 font-bold">Category</p>
+//     <ul className="text-sm leading-5">
+//       {
+//         !category 
+//         ? `Loading...`
+//         : category.map((category) => {
+//           return ( <li key={category.id}>
+//           <Link href={`/category/${category.id}`} legacyBehavior>
+//             <a className="block py-2 my-1 pl-2 hover:bg-hover">{category.name}</a>
+//           </Link>
+//         </li>
+//         );
+//       })}
+//     </ul>
+//   </div>
+//   );
+// };
 
-export const getStaticProps = async () => {
-  // カテゴリーコンテンツの取得
-  const categoryData = await client.get({ endpoint: "categories" });
-  console.log(categoryData);
+// export const getStaticProps = async () => {
+//   // カテゴリーコンテンツの取得
+//   const categoryData = await client.get({ endpoint: "categories" });
+//   console.log(categoryData);
 
-  return {
-    props: {
-      category: categoryData.contents,
-    },
-  };
-};
+//   return {
+//     props: {
+//       category: categoryData.contents,
+//     },
+//   };
+// };
--- src/pages/tag/[id].jsx
+++ src/pages/tag/[id].jsx
@@ -1,41 +1,41 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-const tagId = ({ blog }) => {
-  // タグに紐付いたコンテンツがない場合に表示
-  if (blog.length === 0) {
-    return <div>ブログコンテンツがありません</div>;
-  }
-  return (
-    <div>
-      <ul>
-        {blog.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+// const tagId = ({ blog }) => {
+//   // タグに紐付いたコンテンツがない場合に表示
+//   if (blog.length === 0) {
+//     return <div>ブログコンテンツがありません</div>;
+//   }
+//   return (
+//     <div>
+//       <ul>
+//         {blog.map((blog) => (
+//           <li key={blog.id}>
+//             <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
-// 静的生成のためのパスを指定します
-export const getStaticPaths = async () => {
-  const data = await client.get({ endpoint: "tags" });
-  const paths = data.contents.map((content) => `/tag/${content.id}`);
+// // 静的生成のためのパスを指定します
+// export const getStaticPaths = async () => {
+//   const data = await client.get({ endpoint: "tags" });
+//   const paths = data.contents.map((content) => `/tag/${content.id}`);
 
-  return { paths, fallback: false };
-};
+//   return { paths, fallback: false };
+// };
 
-// データをテンプレートに受け渡す部分の処理を記述します
-export const getStaticProps = async (context) => {
-  const id = context.params.id;
-  const data = await client.get({ endpoint: "blog", queries: { filters: `tags[contains]content_id` } });
+// // データをテンプレートに受け渡す部分の処理を記述します
+// export const getStaticProps = async (context) => {
+//   const id = context.params.id;
+//   const data = await client.get({ endpoint: "blog", queries: { filters: `tags[contains]content_id` } });
 
-  return {
-    props: {
-      blog: data.contents,
-    },
-  };
-};
+//   return {
+//     props: {
+//       blog: data.contents,
+//     },
+//   };
+// };
 
-export default tagId;
+// export default tagId;
